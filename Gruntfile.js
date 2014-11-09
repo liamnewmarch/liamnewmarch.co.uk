3778bf75ae7e47da6e8d074afd601b2573ee8222
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2014-11-09 16:56:10 +0000

    Use load-grunt-config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,43 +1,5 @@
 module.exports = function(grunt) {
+	'use strict';
 
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-
-		autoprefixer: {
-			build: {
-				src: 'assets/css/min.css',
-				dest: 'assets/css/min.css'
-			}
-		},
-
-		sass: {
-			build: {
-				options: {
-					style: 'compressed'
-				},
-				src: 'assets/css/main.scss',
-				dest: 'assets/css/min.css'
-			}
-		},
-
-		uglify: {
-			build: {
-				options: {
-					mangle: false
-				},
-				files: {
-					'assets/js/min.js': [
-						'bower_components/jquery/dist/jquery.min.js',
-						'assets/js/main.js'
-					]
-				}
-			}
-		},
-	});
-
-	grunt.loadNpmTasks('grunt-autoprefixer');
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-
-	grunt.registerTask('default', ['sass', 'uglify', 'autoprefixer']);
+	require('load-grunt-config')(grunt);
 };
