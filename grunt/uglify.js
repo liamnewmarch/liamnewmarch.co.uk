9234273e49651c599bcc198d0795dfe9e02e456d
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2014-11-10 13:10:15 +0000

    Remove jQuery dependancy

--- grunt/uglify.js
+++ grunt/uglify.js
@@ -1,11 +1,11 @@
 module.exports = {
 	build: {
 		options: {
-			mangle: false
+			mangle: false,
+			sourceMap: true
 		},
 		files: {
 			'assets/js/min.js': [
-				'bower_components/jquery/dist/jquery.min.js',
 				'assets/js/main.js'
 			]
 		}
