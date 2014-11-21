4329bae4964491a029c31778326466173e48833e
Liam Newmarch <liam@liamnewmarch.co.uk>
2014-11-21 14:27:00 +0000

    Add javascript uris to angular's whitelist

--- assets/js/modules/app.js
+++ assets/js/modules/app.js
@@ -1,6 +1,12 @@
 (function() {
     'use strict';
 
-    angular.module('app', []);
+    angular.module('app', []).config(config);
+
+    config.$inject = [ '$compileProvider' ];
+
+    function config($compileProvider) {
+        $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|javascript):/);
+    }
 
 }());
