4e3ef8de8e50cba6813e2b8e939d64352d0a98ee
Liam Newmarch <liam@liamnewmarch.co.uk>
2016-03-13 11:53:35 +0000

    Configure templates, serve static assets

--- blog/urls.py
+++ blog/urls.py
@@ -6,16 +6,27 @@ session_csrf.monkeypatch()
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'blog.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+from blog.views import (
+    home_view,
+    about_view,
+    contact_view,
+    post_view,
+)
+
+urlpatterns = patterns(
+    '',
     url(r'^_ah/', include('djangae.urls')),
-
-    # Note that by default this is also locked down with login:admin in app.yaml
     url(r'^admin/', include(admin.site.urls)),
-
     url(r'^csp/', include('cspreports.urls')),
-
     url(r'^auth/', include('djangae.contrib.gauth.urls')),
+
+    # pages
+    url(r'^$', home_view),
+    url(r'^about/', about_view),
+    url(r'^contact/', contact_view),
+    # posts
+    url(r'^posts/(?P<post_slug>\w+)', post_view),
+    # posts (old url style)
+    # url(r'^(?P<username>\w+)', post_view),
+
 )
