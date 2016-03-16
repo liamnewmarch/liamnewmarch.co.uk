02a3930df541771ec4264d6c5ed8628e242bef51
Liam Newmarch <liam@liamnewmarch.co.uk>
2016-03-16 14:59:37 +0000

    Adjust CSP settings

--- blog/settings.py
+++ blog/settings.py
@@ -124,12 +124,12 @@ if DEBUG:
 
 # sensible default CPS settings, feel free to modify them
 CSP_DEFAULT_SRC = ("'self'", "*.gstatic.com")
-CSP_STYLE_SRC = ("'self'", "fonts.googleapis.com", "*.gstatic.com")
-CSP_FONT_SRC = ("'self'", "themes.googleusercontent.com", "*.gstatic.com")
-CSP_FRAME_SRC = ("'self'", "www.google.com", "www.youtube.com", "accounts.google.com", "apis.google.com", "plus.google.com")
-CSP_SCRIPT_SRC = ("'self'", "*.googleanalytics.com", "*.google-analytics.com", "ajax.googleapis.com")
-CSP_IMG_SRC = ("'self'", "data:", "s.ytimg.com", "*.googleusercontent.com", "*.gstatic.com")
-CSP_CONNECT_SRC = ("'self'", "plus.google.com", "www.google-analytics.com")
+CSP_STYLE_SRC = ("'self'", "*.googleapis.com", "*.gstatic.com")
+CSP_FONT_SRC = ("'self'", "*.gstatic.com")
+CSP_FRAME_SRC = ("'self'", "*.google.com", "disqus.com")
+CSP_SCRIPT_SRC = ("'self'", "*.googleanalytics.com", "*.google-analytics.com", "ajax.googleapis.com", "*.disqus.com")
+CSP_IMG_SRC = ("'self'", "*.google-analytics.com", "*.disqus.com")
+CSP_CONNECT_SRC = ("'self'", "*.google-analytics.com")
 
 
 from djangae.contrib.gauth.settings import *
