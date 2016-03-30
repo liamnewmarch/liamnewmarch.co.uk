6558af4d515008aa3bcf169614ff31a181d048da
Liam Newmarch <liam@liamnewmarch.co.uk>
2016-03-31 00:28:20 +0100

    Allow HTTP, expand allowed hosts

--- blog/settings_live.py
+++ blog/settings_live.py
@@ -6,7 +6,7 @@ SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_FRAME_DENY = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_BROWSER_XSS_FILTER = True
-SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = False
 
 
 SECURE_REDIRECT_EXEMPT = [
@@ -19,3 +19,5 @@ SECURE_CHECKS += ["blog.checks.check_csp_sources_not_unsafe"]
 
 DEBUG = False
 TEMPLATE_DEBUG = False
+
+ALLOWED_HOSTS = [".appspot.com", ".liamnewmarch.co.uk", ".liamnewmarch.com"]
