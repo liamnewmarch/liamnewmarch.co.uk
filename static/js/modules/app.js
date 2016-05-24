e08875edaa0696245179a831261aa7a92c8d8933
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2016-05-24 12:29:31 +0100

    Implement side menu.

--- static/js/modules/app.js
+++ static/js/modules/app.js
@@ -7,3 +7,22 @@ function config($compileProvider) {
 }
 
 angular.bootstrap(document, ['app']);
+
+
+/**
+ * TODO move to component
+ */
+var page = document.querySelector('.page')
+var toggle = document.querySelector('.page__toggle-menu');
+
+page.addEventListener('click', function(event) {
+  if (event.target === event.currentTarget) {
+    event.stopPropagation();
+    page.classList.remove('page--menu-open');
+  }
+});
+
+toggle.addEventListener('click', function(event) {
+  event.stopPropagation();
+  page.classList.toggle('page--menu-open');
+});
