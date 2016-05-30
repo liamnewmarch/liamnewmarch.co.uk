73f7a6600dbed61f4ed560ce5bbcfe6656abe8f5
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2016-05-30 13:36:57 +0100

    Convert grunt task files to yaml.

--- grunt/autoprefixer.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  options: {
-    map: true
-  },
-  build: {
-    src: 'static/css/min.css',
-    dest: 'static/css/min.css'
-  }
-};
--- grunt/exorcise.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  build: {
-    files: {
-      'static/js/min.js.map': ['static/js/min.js']
-    }
-  }
-};
