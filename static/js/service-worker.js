2db68e3c7428c72afd5a23dcaa8186e77157c0d3
Liam Newmarch <liam@liamnewmarch.co.uk>
2016-06-20 21:11:57 +0100

    Add dynamic caching to service worker.

--- static/js/service-worker.js
+++ static/js/service-worker.js
@@ -1,20 +1,24 @@
-const cacheVersion = '1.0.0';
+const version = '1.0.0';
 
-self.addEventListener('install', event => {
-  console.log('install!');
-  event.waitUntil(caches.open(cacheVersion).then(cache => {
-    return cache.addAll([
-      '/static/css/min.css',
-      '/static/js/min.js',
-      '/static/img/avatar.jpg',
-      '/',
-    ]);
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => {
+    const oldKeys = keys.filter(key => key.includes(version));
+    return oldKeys.map(key => caches.delete(key));
   }));
 });
 
 self.addEventListener('fetch', event => {
-  console.log('fetch!');
+  const url = new URL(event.request.url);
+  if (url.protocol === 'data:') {
+    return fetch(event.request);
+  }
   event.respondWith(caches.match(event.request).then(response => {
-    return response || fetch(event.request);
+    return response || fetch(event.request).then(response => {
+      const clone = response.clone();
+      caches.open(version).then(cache => {
+        cache.put(event.request, clone);
+      });
+      return response;
+    });
   }));
 });
