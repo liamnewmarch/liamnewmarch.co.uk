9360797948ada51cc4e03f3663fbcd0c911ce445
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2016-07-07 23:35:01 +0100

    Don’t wait for DOM load event when using async.

--- static/js/utils/register-component.js
+++ static/js/utils/register-component.js
@@ -1,14 +1,9 @@
 const registered = [];
 
-export function registerComponent(component) {
-  registered.push(component);
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-  registered.forEach(Component => {
-    const elements = document.querySelectorAll(Component.selector);
-    [].forEach.call(elements, (element) => {
-      new Component(element);
-    });
+export function registerComponent(Component) {
+  const elements = document.querySelectorAll(Component.selector);
+  [].forEach.call(elements, (element) => {
+    new Component(element);
   });
-});
+  registered.push(Component);
+}
