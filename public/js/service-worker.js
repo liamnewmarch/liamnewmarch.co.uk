88fce66503f3850aa24ea406c2586bb7dd86cb74
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2018-10-07 18:36:31 +0100

    Add wave canvas and hue rotate 😱

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -1,25 +1,42 @@
 const version = '2.0.0';
 
-self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => {
+const files = [
+  '/',
+  '/css/main.css',
+  '/img/logo.svg',
+  '/js/app.js',
+];
+
+class OfflineStrategy {
+  static async activate() {
+    const keys = await caches.keys();
     const oldKeys = keys.filter(key => key.includes(version));
     return oldKeys.map(key => caches.delete(key));
-  }));
+  }
+
+  static async fetch(event) {
+    const cached = await caches.match(event.request);
+    const network = await fetch(event.request);
+    const clone = network.clone();
+    const cache = await caches.open(version);
+    cache.put(event.request, clone);
+    return cached || network;
+  }
+
+  static async install() {
+    const cache = await caches.open(version);
+    cache.addAll(files);
+  }
+}
+
+self.addEventListener('activate', event => {
+  event.waitUntil(OfflineStrategy.activate(event));
 });
 
 self.addEventListener('fetch', event => {
-  const url = new URL(event.request.url);
-  if (url.protocol === 'data:') {
-    return fetch(event.request);
-  }
-  event.respondWith(caches.match(event.request).then(cached => {
-    const network = fetch(event.request).then(response => {
-      const clone = response.clone();
-      caches.open(version).then(cache => {
-        cache.put(event.request, clone);
-      });
-      return response;
-    });
-    return cached || network;
-  }));
+  event.respondWith(OfflineStrategy.fetch(event));
+});
+
+self.addEventListener('install', event => {
+  event.waitUntil(OfflineStrategy.install(event));
 });
